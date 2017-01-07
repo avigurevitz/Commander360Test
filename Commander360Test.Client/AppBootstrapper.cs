30a35a21ab7550e4e4bf5f23167c57d311938a7e
Avig <USER@AVIG>
2017-01-07 21:19:55 +0200

    update

---
+++ Commander360Test.Client/AppBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace Commander360Test.Client
+{ 
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<Shell.ShellViewModel>();
+        }
+    }
+}
+
